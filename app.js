500cc155971ffe0583a0f165eaaa03aece22b50f
GallonShih <gallonshih0526@gmail.com>
2022-01-30 23:11:46 +0800

    feet: login project initialized

---
+++ app.js
@@ -0,0 +1,20 @@
+// require packages
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const routes = require('./routes')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// setting
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(routes)
+
+// start and listen on the Express server
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
